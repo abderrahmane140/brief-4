682bb24f9b78cd2333ac9db04549c662038f68be
abderrahmane140 <bsarabderrahmane01@gmail.com>
2025-10-29 10:18:55 +0100

    passing the day to the from input

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const days = document.querySelectorAll('#day');
 const model = document.getElementById('model');
 const closeBtn = document.getElementById('closeBtn');
+const reservation_day = document.getElementById('reservation-day');
 
 
 
@@ -9,13 +10,15 @@ days.forEach((e) => {
     console.log(e)
     e.addEventListener('click',()=>{
         const span = e.querySelector('span')
-        openModel(span.id);
+        openModel(span.id, e);
     })
 })
 
 function openModel(id){
     model.classList.remove("hidden");
     model.classList.add("flex");
+
+    reservation_day.value = id;
 }
 
 function closeModel(){
