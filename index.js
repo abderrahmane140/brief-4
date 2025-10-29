559535eeb940fc92129087afd178faddaad62544
abderrahmane140 <bsarabderrahmane01@gmail.com>
2025-10-29 09:49:31 +0100

    add functionality for the close and open model

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const days = document.querySelectorAll('#day');
+const model = document.getElementById('model');
+const closeBtn = document.getElementById('closeBtn');
+
+
+
 
 days.forEach((e) => {
     console.log(e)
     e.addEventListener('click',()=>{
         const span = e.querySelector('span')
-        console.log(span.id)
+        openModel(span.id);
     })
 })
+
+function openModel(id){
+    model.classList.remove("hidden");
+    model.classList.add("flex");
+}
+
+function closeModel(){
+    model.classList.remove("flex");
+    model.classList.add("hidden");
+}
+
+closeBtn.addEventListener('click', closeModel);
+
+
